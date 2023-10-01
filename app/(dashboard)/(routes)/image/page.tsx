9a4609cc4e8aeeda0d33c4d01b647e1f56644efe
Danial Ahmad <108906119+iamdanialahmad@users.noreply.github.com>
2023-10-01 05:47:43 +0500

    feature: Error handling using toast and landing page design completed

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -54,6 +54,9 @@ const PhotoPage = () => {
       if (error?.response?.status === 403) {
         proModal.onOpen();
       }
+      else {
+        toast.error("Something went wrong");
+      }
     } finally {
       router.refresh();
     }
